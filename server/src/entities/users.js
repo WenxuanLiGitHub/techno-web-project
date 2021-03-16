05e19e8362301bbbf316d9e2ec041ada6056c494
Wenxuan Li <71944101+WenxuanLiGitHub@users.noreply.github.com>
2021-03-16 17:06:44 +0100

    Fix post and get of user

--- server/src/entities/users.js
+++ server/src/entities/users.js
@@ -5,43 +5,36 @@ class Users {
   }
 
   create(login, password, lastname, firstname) {
+    let _this = this
     return new Promise((resolve, reject) => {
-      let userid = 1; // À remplacer par une requête bd
-
-      if(false) {
-        //erreur
-        reject();
-      } else {
-        resolve(userid);
-      }
+      var stmt = _this.db.prepare("INSERT INTO users VALUES (?,?,?,?)")
+      stmt.run([login, password, lastname, firstname], function(err, res){
+        if (err){
+          reject(err);
+        }else{
+          resolve(this.lastID);    
+        }
+      })
     });
   }
 
-  get(userid) {
+  async get(userid) {
     return new Promise((resolve, reject) => {
-      const user = {
-         login: "pikachu",
-         password: "1234",
-         lastname: "chu",
-         firstname: "pika"
-      }; // À remplacer par une requête bd
-
-      if(false) {
-        //erreur
-        reject();
-      } else {
-        if(userid == 1) {
-          resolve(user);
-        } else {
-          resolve(null);
+      var stmt = this.db.prepare("SELECT * FROM users WHERE rowid = ?")
+      stmt.get([userid], function(err, res){
+        if (err){
+          reject(err);
+        }else{
+          resolve(res);
         }
-      }
+      })
     });
   }
 
   async exists(login) {
     return new Promise((resolve, reject) => {
-      var stmt = db.prepare("SELECT login FROM users WHERE login = ?")
+      console.log("focntion user exists entrée")
+      var stmt = this.db.prepare("SELECT login FROM users WHERE login = ?")
       stmt.get([login], function(err, res){
         if(err) {
           reject(err)
@@ -52,7 +45,7 @@ class Users {
     });
   }
 
-  checkpassword(login, password) {
+  async checkpassword(login, password) {
     return new Promise((resolve, reject) => {
       let userid = 1; // À remplacer par une requête bd
       if(false) {
